efca284d6912d68584d4b9d3100b49e41a3152a3
krishnadeveloper <getkrishnakumar1990@gmail.com>
2018-04-15 19:00:23 +0530

    Create base64-code-of-image-from-php-code.php
    Create the base64 code of an image using php code

---
+++ base64-code-of-image-from-php-code.php
@@ -0,0 +1,4 @@
+$path = 'myfolder/myimage.png';
+$type = pathinfo($path, PATHINFO_EXTENSION);
+$data = file_get_contents($path);
+$base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
